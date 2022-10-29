158ad273614e7cf1bd78b97eef55eff41b388729
ak2572 <ak2572@njit.edu>
2022-10-29 15:50:38 -0400

    Protect the profile route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-import LoginButton from "./components/LoginButton";
-import LogoutButton from "./components/LogoutButton";
-import {useAuth0} from "@auth0/auth0-react";
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import { withAuthenticationRequired, useAuth0 } from '@auth0/auth0-react';
 import {
-  BrowserRouter,
   Routes,
-  Route,
+  Route
 } from 'react-router-dom';
 import Leaderboard from './components/Leaderboard';
 import Profile from './components/Profile';
 import Game from './components/Game';
 
+const ProtectedRoute = ({ component, ...args }) => {
+  const Component = withAuthenticationRequired(component, args);
+  return <Component />;
+};
+
 function App() {
-  const { isLoading, error} = useAuth0();
-    return (
-    <BrowserRouter>
+  const { isLoading, error } = useAuth0();
+  return (
+    <div>
       <Routes>
         <Route path='/' element={<div>quicktype</div>}></Route>
         <Route path='/leaderboard' element={<Leaderboard />}></Route>
-        <Route path='/profile/:id' element={<Profile />}></Route>
+        <Route path='/profile' element={<ProtectedRoute component={Profile} />}></Route>
         <Route path='/game' element={<Game />}></Route>
       </Routes>
-      <main className="column">
+      <main className='column'>
         <h1>QuickType Login</h1>
         {error && <p>Authentication Error</p>}
         {!error && isLoading && <p>Loading...</p>}
         {!error && !isLoading && (
-            <>
-              <LoginButton />
-              <LogoutButton />
-              <Profile />
-            </>
+          <>
+            <LoginButton />
+            <LogoutButton />
+          </>
         )}
-
       </main>
-    </BrowserRouter>
+    </div>
   );
 }
 
--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,60 +1,58 @@
 import React, { useEffect, useState } from 'react';
-import {useAuth0} from "@auth0/auth0-react";
+import {useAuth0} from '@auth0/auth0-react';
 import { useParams } from 'react-router-dom';
 
 const Profile = () => {
-  const { id } = useParams();
+  // const { id } = useParams();
   const { user, isAuthenticated } = useAuth0();
 
-  const [scores, setScores] = useState([]);
+  // const [scores, setScores] = useState([]);
 
-  const getScores = async () => {
-    try {
-      const response = await fetch(`http://localhost:5000/api/scores/${id}`);
-      const jsonData = await response.json();
+  // const getScores = async () => {
+  //   try {
+  //     const response = await fetch(`http://localhost:5000/api/scores/${id}`);
+  //     const jsonData = await response.json();
 
-      setScores(jsonData);
-    } catch (err) {
-      console.error(err);
-    }
-  }
+  //     setScores(jsonData);
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // }
 
-  useEffect(() => {
-    getScores();
-  }, []);
+  // useEffect(() => {
+  //   getScores();
+  // }, []);
 
   return (
-      isAuthenticated && (
-            <article className='column'>
-                {user?.picture && <img src={user.picture} alt={user?.name} />}
-                <h2>{user?.name}</h2>
-                <ul>
-                    {Object.keys(user).map((objKey, i) => <li key={i}>{objKey}: {user[objKey]} </li>)}
-                </ul>
-                 <div>
-      <h1>Profile</h1>
-      <h2>Past Scores</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>User ID</th>
-            <th>WPM</th>
-          </tr>
-        </thead>
-        <tbody>
-          {scores.map((score) => (
-            <tr>
-              <td>{score.user_id}</td>
-              <td>{score.score}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-            </article>
-
-        )
-
+    isAuthenticated && (
+      <article className='column'>
+        {user?.picture && <img src={user.picture} alt={user?.name} />}
+        <h2>{user?.name}</h2>
+        <ul>
+          {Object.keys(user).map((objKey, i) => <li key={i}>{objKey}: {user[objKey]} </li>)}
+        </ul>
+        <div>
+          <h1>Profile</h1>
+          <h2>Past Scores</h2>
+          {/* <table>
+            <thead>
+              <tr>
+                <th>User ID</th>
+                <th>WPM</th>
+              </tr>
+            </thead>
+            <tbody>
+              {scores.map((score) => (
+                <tr>
+                  <td>{score.user_id}</td>
+                  <td>{score.score}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table> */}
+        </div>
+      </article>
+    )
   )
 }
 
