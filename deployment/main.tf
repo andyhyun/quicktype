d22b6cc34e55a88b03f0fde50ad572c6491aadb7
pavly <pr253@njit.edu>
2022-11-04 17:57:10 -0400

    edited terraform file

--- deployment/main.tf
+++ deployment/main.tf
@@ -55,8 +55,29 @@ resource "aws_ecs_service" "quicktype-frontend-service" {
   task_definition        = aws_ecs_task_definition.quicktype-frontend-task-definition.id
   desired_count          = 5
   network_configuration {
-    subnets          = ["subnet-0b8f437d046a9d818"]
-    security_groups  = ["sg-057af67719d0de21b"]
+    subnets          = ["subnet-04543979c75187933"]
+    security_groups  = ["sg-0d4fb5f077b4a953d"]
     assign_public_ip = true
+  }
+}
+
+# define a load balancer
+resource "aws_lb" "quicktype-frontend-lb" {
+  name               = "quicktype-frontend-lb"
+  internal           = false
+  ip_address_type    = "ipv4"
+  load_balancer_type = "application"
+  security_groups    = ["sg-0d4fb5f077b4a953d"]
+  subnets = ["subnet-00da83bc2f50636db","subnet-01524a7fe6bf7c90d","subnet-04543979c75187933","subnet-045fa5fa8fb035456","subnet-088a21ebe1445fd7d","subnet-0aff7575a5132c4d9"]
+}
+
+# define a listener
+resource "aws_lb_listener" "alb-listener" {
+  load_balancer_arn = "arn:aws:elasticloadbalancing:us-east-1:800636676873:loadbalancer/app/quicktype-frontend-lb/9ceb532550000d9a"
+  port              = 443
+  protocol          = "HTTP"
+  default_action {
+    target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:800636676873:targetgroup/quicktype-frontend-tg/09fff9b54d39cbb5"
+    type             = "forward"
   }
 }
